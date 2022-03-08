f6488bc7423dc937ad6370a69fcaca9eb33afad8
AmeerHamza4 <hamzaameers196@gmail.com>
2022-03-08 22:15:39 +0500

    second project

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int a;
+	cout << "please enter a Number : ";
+	cin >> a;
+	if (a % 2 == 0)
+		cout << a << " is even number";
+	else 
+		cout << a<<" is odd number";
+	}
+	
+
+
+
+
+
+
